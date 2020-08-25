093052510001aecccfe1d4456d8790d86b57242e
Reyadul Islam <reyadul.islam792@gmail.com>
2020-08-25 12:18:06 +0600

    module

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Counter></Counter>
+        <Users></Users>
+
+
+
+      <p>Learning sometime so tough </p>
+
+
       </header>
+     </div>  
+    
+  )
+}
+//main above  ---->
+
+
+
+
+function Counter(){
+
+  const[count,setCount]=useState(0);
+  const handleIncrease=()=>setCount(count+1);
+
+  return(
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
-  );
+  )
+}
+function Users(){
+    const[users,setUsers]=useState([]);
+    useEffect(()=>{ 
+
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(p=>p.json())
+      .then(l=>setUsers(l));
+
+    },[])
+
+  return(
+    <div>
+      <h3>Dynamic Users:{users.length}</h3>
+      <ul>
+        {
+          console.log(users)
+        }
+        {
+          users.map(user=><li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
 }
 
 export default App;
